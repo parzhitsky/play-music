f90d05276f30e5db213747621090da82c695a902
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-10-02 00:25:09 +0300

    Extract `Fn` type

--- packages/playground/src/play/memoized.ts
+++ packages/playground/src/play/memoized.ts
@@ -1,10 +1,13 @@
-export function memoized<Inputs extends readonly (string | number)[], Output>(fn: (...inputs: Inputs) => Output): (...inputs: Inputs) => Output {
-  const delimiter = Math.random().toString(36).slice(2)
+type Fn<Inputs extends readonly unknown[], Output> = (...inputs: Inputs) => Output
+
+export function memoized<Inputs extends readonly number[], Output>(fn: Fn<Inputs, Output>): Fn<Inputs, Output> {
+  const sep = Math.random().toString(36).slice(2)
   const memo = Object.create(null) as Record<string, Output>
 
   return (...inputs) => {
-    const key = inputs.join(delimiter)
+    const key = inputs.join(sep)
+    const value = memo[key] ??= fn(...inputs)
 
-    return memo[key] ??= fn(...inputs)
+    return value
   }
 }
