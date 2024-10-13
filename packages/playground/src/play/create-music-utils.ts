ac7ed2daa84be67b08bc9b9f6318b775d73070e2
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-10-13 11:22:21 +0300

    Remove premature optimization

--- packages/playground/src/play/create-music-utils.ts
+++ packages/playground/src/play/create-music-utils.ts
@@ -1,6 +1,5 @@
 import { calculateFrequency } from "./calculate-frequency.js"
 import { Sound, VoiceLineItem, Pause, VoiceLine } from "./create-music.js"
-import { memoized } from "./memoized.js"
 
 export { Sound, Pause, VoiceLineItem, VoiceLine, VoiceLines, Music } from './create-music.js'
 
@@ -9,9 +8,9 @@ export { Sound, Pause, VoiceLineItem, VoiceLine, VoiceLines, Music } from './cre
  */
 export const _ = NaN
 
-export const sound = memoized((duration: number, frequency: number): Sound => {
+export function sound(duration: number, frequency: number): Sound {
   return [duration, frequency]
-})
+}
 
 function isVoiceLineItem(input: unknown): input is VoiceLineItem {
   return input instanceof Array && input.length >= 2 && typeof input[0] === 'number'
@@ -21,17 +20,17 @@ export function isSound(item: VoiceLineItem): item is Sound {
   return isVoiceLineItem(item) && typeof item[1] === 'number'
 }
 
-export const pause = memoized((duration: number): Pause => {
+export function pause(duration: number): Pause {
   return sound(duration, _)
-})
+}
 
 export function isPause(item: VoiceLineItem): item is Pause {
   return isSound(item) && Object.is(_, item[1])
 }
 
-export const note = memoized((duration: number, semitonesFromBase: number): Sound => {
+export function note(duration: number, semitonesFromBase: number): Sound {
   return sound(duration, calculateFrequency(semitonesFromBase))
-})
+}
 
 export function notes(duration: number, semitonesFromBase: number[]): VoiceLine {
   return semitonesFromBase.map((distance) => note(duration, distance))
