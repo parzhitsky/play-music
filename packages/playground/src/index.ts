d60d63a1153e0da58f5d6b733debd1f54cc6dde8
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2024-11-22 20:01:36 +0200

    Provide a way to soften an abrupt stop

--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -1,4 +1,6 @@
 import { bachPrelude1 } from './bach-prelude-1.music.js'
 import { play } from './play/play.js'
 
-await play(bachPrelude1)
+await play(bachPrelude1, {
+  endCushionMsec: 100,
+})
--- packages/playground/src/play/play.ts
+++ packages/playground/src/play/play.ts
@@ -5,12 +5,22 @@ import { MusicInterpreter } from './music-interpreter.js'
 interface PlayParams {
   readonly startFromLabel?: string
   readonly playUntilLabel?: string
+
+  /**
+   * The amount of time to keep oscillators attached to the audio context after the voice had ended
+   */
+  readonly endCushionMsec?: number
 }
 
 export async function play(music: Music, {
   startFromLabel: startLabel,
   playUntilLabel: stopLabel,
+  endCushionMsec = 0,
 }: PlayParams = {}): Promise<void> {
+  if (endCushionMsec < 0 || !Number.isFinite(endCushionMsec)) {
+    throw new Error('End cushion must be a finite non-negative number')
+  }
+
   const audioContext = new AudioContext()
   const interpreter = new MusicInterpreter()
 
@@ -26,7 +36,14 @@ export async function play(music: Music, {
       throw new Error(`Start label ("${startLabel}") must come strictly earlier than stop label ("${stopLabel}")`)
     }
 
-    const played = voices.map((voice): Promise<void> => {
+    const played = voices.map(async (voice): Promise<void> => {
+      // time of the item that is after the last item of the voice
+      const extraItemTime = voice.duration - startTime
+
+      if (extraItemTime < 0) {
+        return
+      }
+
       const oscillator = new OscillatorNode(audioContext, { type: 'sine' })
       const gain = new GainNode(audioContext, { gain: 0.25 })
 
@@ -36,10 +53,12 @@ export async function play(music: Music, {
         oscillator.frequency.setValueAtTime(frequency, itemTimeShifted)
       }
 
+      oscillator.frequency.setValueAtTime(0, extraItemTime)
+
       const durationReduced = Math.min(stopTime - startTime, voice.duration)
 
       oscillator.start(audioContext.currentTime)
-      oscillator.stop(audioContext.currentTime + durationReduced)
+      oscillator.stop(audioContext.currentTime + durationReduced + (endCushionMsec / 1000))
       oscillator.connect(gain).connect(audioContext.destination)
 
       return new Promise((resolve) => {
